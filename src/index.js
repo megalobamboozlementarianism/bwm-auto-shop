b8d8e8dac73fcd8865c09c7c0b65defdc62cc5f9
megalobamboozlementarianism <abe.coley@gmail.com>
2021-07-19 12:22:35 -0600

    added hscraper

--- src/index.js
+++ src/index.js
@@ -1,19 +1,49 @@
 const express = require("express")
+const path = require('path')
 const wwwhisper = require('connect-wwwhisper')
 const { chromium } = require("playwright-chromium")
 const { firefox } = require("playwright-firefox")
-
+const port = process.env.PORT || 3000;
 const app = express()
 app.use(wwwhisper())
 app.use(express.static("./public"))
-const port = process.env.PORT || 3000;
+
+
+
+app.get('/hscrape', async (req, res) => {
+    const url = req.query.url
+    console.log(`Incoming request for URL '${url}'`)
+      try {
+        
+        /** @type {import('playwright-chromium').Browser} */
+        const browser = await chromium.launch({
+          chromiumSandbox: false
+        })
+        let context = await browser.newContext({viewport: { width: 800, height: 1200 }})
+        page = await context.newPage();
+        
+        await page.goto(url)
+  
+        if (req.query.timeout) {
+          await page.waitForTimeout(parseInt(req.query.timeout, 10))
+        }
+        const h1s = await page.$$eval('h1', hOnes => hOnes.map(h1 => ` ${h1.innerText}`))
+        const h2s = await page.$$eval('h2', hTwos => hTwos.map(h2 => ` ${h2.innerText}`))
+        await browser.close()
+        res.json({h1s: h1s, h2s: h2s })
+      } catch (err) {
+        res.status(500).send(`Something went wrong: ${err}`)
+      }
+  });
 
 app.get("/browser/:name", async (req, res) => {
+  const keys = Object.entries(req.query)
+  console.log("req: " + keys)
   const browserName = req.params["name"] || "chromium"
   if (!["chromium", "firefox"].includes(browserName)) {
     return res.status(500).send(`invalid browser name (${browserName})!`)
   }
-  const url = req.query.url || "https://bigwestmarketing.com"
+  const url = req.query.url
   const waitUntil = req.query.waitUntil || "load"
   const width = req.query.width ? parseInt(req.query.width, 10) : 1920
   const height = req.query.height ? parseInt(req.query.height, 10) : 1080
