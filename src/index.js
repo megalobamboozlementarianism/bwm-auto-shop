d189e7438acbb246c0f245be5b1da9d1b0b7cfc6
megalobamboozlementarianism <abe.coley@gmail.com>
2021-07-19 16:35:50 -0600

    fixed bugs, added monthlies

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ app.use(express.json())
 
 
 app.post('/hlist', async (req, res) => {
-  console.log("hi from /hlist")
   const body = req.body
     try {
       /** @type {import('playwright-chromium').Browser} */
@@ -20,16 +19,66 @@ app.post('/hlist', async (req, res) => {
       let context = await browser.newContext({viewport: { width: 800, height: 1200 }})
       page = await context.newPage();
       let output = []
-      console.log("made it just before for-loop")
       for (let i = 0; i < body.length; i++){
         console.log(`Incoming request for URL '${body[i]}'`)
         await page.goto(body[i])
+        // get title
+        const title = await page.title()
+        // get h1s and h2s
         const h1s = await page.$$eval('h1', hOnes => hOnes.map(h1 => ` ${h1.innerText}`))
         const h2s = await page.$$eval('h2', hTwos => hTwos.map(h2 => ` ${h2.innerText}`))
+        // get schema
+        let schema_data;
+        const scripts = await page.$$('script')
+        for (let j = 0; j < scripts.length; j++) {
+          let type = await scripts[j].getAttribute("type")
+          if (type === "application/ld+json") {
+            let obj = await scripts[j].innerText()
+            if (obj.includes(`"@type": "LocalBusiness"`)) {
+              let data = JSON.parse(obj)
+              let clean = JSON.stringify(data, null, 4);
+              schema_data = clean
+            }
+          }
+        }
+        // get socials
+        console.log("made it to social check")
+        const ayys = await page.$$eval('a', links => links.map(a => JSON.parse(`{"status": "", "href": "${a.href}"}`)));
+        console.log("got the ayys")
+        let socials = []
+        ayys.forEach(elem => {
+          if (elem.href.includes("facebook")) { socials.push(elem) }
+          else if (elem.href.includes("yelp")) { socials.push(elem) }
+          else if (elem.href.includes("instagram")) { socials.push(elem) }
+          else if (elem.href.includes("youtube")) { socials.push(elem) }
+          else if (elem.href.includes("google")) { socials.push(elem) }
+          else if (elem.href.includes("homeadvisor")) { socials.push(elem) }
+        })
+        let clean_socials = new Set(socials);
+        console.log("cleans")
+        console.log(clean_socials)
+        for (let elem of clean_socials) {
+          let socialpage = await context.newPage();
+          try {
+            let res = await socialpage.goto(elem.href, 0)
+            let code = res.status();
+            elem.status = code
+          } catch (error) {
+            elem.status = 404
+          }
+          socialpage.close();
+        }
+
+        let socials_status = JSON.stringify(Array.from(clean_socials));
+        console.log(socials_status)
+        // add output to res json
         output.push({
+          "title": title,
           "site": body[i],
           "h1s": h1s,
-          "h2s": h2s
+          "h2s": h2s,
+          "schema": schema_data,
+          "socials": socials_status
         })
       }
       await browser.close()
